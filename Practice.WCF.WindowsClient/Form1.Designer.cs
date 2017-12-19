cc0ed71475ae0e44b5f13709d1bac8794e827ddd
rajmanioinam <31492859+rajmanioinam@users.noreply.github.com>
2017-12-18 23:29:22 -0700

    Add project files.

---
+++ Practice.WCF.WindowsClient/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Practice.WCF.WindowsClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetMessage = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGetMessage
+            // 
+            this.btnGetMessage.Location = new System.Drawing.Point(122, 14);
+            this.btnGetMessage.Name = "btnGetMessage";
+            this.btnGetMessage.Size = new System.Drawing.Size(108, 23);
+            this.btnGetMessage.TabIndex = 0;
+            this.btnGetMessage.Text = "Get Message";
+            this.btnGetMessage.UseVisualStyleBackColor = true;
+            this.btnGetMessage.Click += new System.EventHandler(this.btnGetMessage_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(13, 48);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblMessage.TabIndex = 1;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(16, 16);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(100, 20);
+            this.txtMessage.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnGetMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetMessage;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtMessage;
+    }
+}
+
---
+++ Practice.WCF.WindowsClient/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice.WCF.WindowsClient
+{
+    public partial class Form1 : Form
+    {
+        PracticeWCFService.PracticeServiceClient client;
+        public Form1()
+        {
+            InitializeComponent();
+            client = new PracticeWCFService.PracticeServiceClient();
+        }
+
+        private void btnGetMessage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(client.State == System.ServiceModel.CommunicationState.Faulted)
+                {
+                    client = new PracticeWCFService.PracticeServiceClient();
+                }
+                lblMessage.Text = client.GetMessage(txtMessage.Text);
+            }
+            catch(Exception ex)
+            {
+                lblMessage.Text = ex.Message.ToString();
+            }
+        }
+    }
+}
